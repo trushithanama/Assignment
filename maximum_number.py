be48d57e94f94214ccd6f8a6ef64140b9ca7f97f
trushithanama <67234326+trushithanama@users.noreply.github.com>
2020-06-22 09:50:09 +0530

    Create maximum_number.py

---
+++ maximum_number.py
@@ -0,0 +1,8 @@
+# max of three numbers
+def max (a,b,c) :
+        if a>b and a>c:
+            print("maximum is :",a)
+        elif b>a and b>c:
+            print("maximum is :",b)
+        else:
+            print("maximum is :",c)
