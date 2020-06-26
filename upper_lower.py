30a978efbe7a03107f84533b5b4da1c1bfd708fe
trushithanama <67234326+trushithanama@users.noreply.github.com>
2020-06-26 09:13:01 +0530

    Create upper_lower.py

---
+++ upper_lower.py
@@ -0,0 +1,11 @@
+#converting string to upper case and lower case
+user_input = input("enter your string: ")
+print("i love my country ", user_input.upper())
+print("i love my country" , user_input.lower())
+
+
+output
+enter your string: trushitha
+i love my country  TRUSHITHA
+i love my country trushitha
+>>> 
