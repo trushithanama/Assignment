6b8022edf252e4afd2a179ebff4399b4c6ef48c9
trushithanama <67234326+trushithanama@users.noreply.github.com>
2020-06-26 09:21:17 +0530

    Create return.py

---
+++ return.py
@@ -0,0 +1,14 @@
+a=input("enter first string: ")
+b=input("enter second string: ")
+def chars_mix_up(a, b):
+    new_a = b[:2] + a[2:]
+    new_b = a[:2] + b[2:]
+    
+return
+print(chars_mix_up(a,b))
+
+
+output
+enter first string:hello
+enter second string:world
+wollo herld
