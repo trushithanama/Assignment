eaf00a6b00cfed7c4531890215d40fed5601ff89
trushithanama <67234326+trushithanama@users.noreply.github.com>
2020-06-27 07:34:30 +0530

    Create length_string.py

---
+++ length_string.py
@@ -0,0 +1,13 @@
+
+#finding length of the string in word refrigerator
+def findlength(string):
+    count=0
+    for i in string:
+         count+=1
+    return count
+string="refrigerator"
+print(findlength(string))
+
+
+output
+12
