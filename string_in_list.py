c1c0677cf5f4a49f62554bcb4efc3fc96d49a28e
trushithanama <67234326+trushithanama@users.noreply.github.com>
2020-06-27 07:15:50 +0530

    Create string_in_list.py

---
+++ string_in_list.py
@@ -0,0 +1,13 @@
+# string in a list
+str1="this is trushitha"
+print(str1.split(' '))
+str1="this is trushitha"
+print(str1.split('_'))
+
+
+
+
+output
+['this', 'is', 'trushitha']
+['this is trushitha']
+>>> 
