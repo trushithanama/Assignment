b1193880aab8468bd5e86c9e37338fc4c02fa343
trushithanama <67234326+trushithanama@users.noreply.github.com>
2020-06-28 19:42:34 +0530

    Create A9_remove_iteam.py

---
+++ A9_remove_iteam.py
@@ -0,0 +1,15 @@
+#python program to remove duplicates from a list
+a = [10,20,30,40,50,60,40,80,50,40]
+
+
+dup_items = set()
+uniq_item = []
+for x in a:
+    if x not in dup_iteam:
+        uniq_iteams.append(x)
+        dup_iteams.add(x)
+        
+print(dup_iteam)
+
+output
+[40,10,80,50,20,60,30]
