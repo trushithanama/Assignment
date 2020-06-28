0f5217fb12dd85737467a78747a50f9e1c239ee6
trushithanama <67234326+trushithanama@users.noreply.github.com>
2020-06-28 19:53:29 +0530

    Create A9_shuffle.py

---
+++ A9_shuffle.py
@@ -0,0 +1,10 @@
+#python program to shuffle and print a specified list.
+from random import shuffle
+color = ['red','green','white','black','pink','yellow',]
+shuffle(color)
+print(color)
+
+
+output
+['yellow', 'black', 'pink', 'white', 'red', 'green']
+>>> 
