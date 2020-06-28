6492a68591b910d68c48c520fe239b7ae2b5fd33
trushithanama <67234326+trushithanama@users.noreply.github.com>
2020-06-28 19:34:49 +0530

    Create A9_multiply_list.py

---
+++ A9_multiply_list.py
@@ -0,0 +1,13 @@
+#python program to multiplies all the items in the list
+def multiply_list(item):
+    tot = 1
+    for x in item:
+        tot *= x
+    return tot
+print("the multiple of item in the list is:",multiply_list([1,2,8,10]))
+
+
+
+output
+the multiple of item in the list is: 160
+>>> 
