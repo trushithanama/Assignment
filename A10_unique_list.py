82278bd88b7eccfa3df137f66cf4af451976d319
trushithanama <67234326+trushithanama@users.noreply.github.com>
2020-06-30 08:23:33 +0530

    Create A10_unique_list.py

---
+++ A10_unique_list.py
@@ -0,0 +1,15 @@
+#python program to find unique numbers from the list
+def unique_list (1) :
+    x = []
+    for a in 1:
+        if a not in x:
+            x.append(a)
+        return x 
+        
+        
+print(unique_list([1,2,3,3,3,3,4,5]))
+
+
+
+output
+[1,2,3,4,5]
