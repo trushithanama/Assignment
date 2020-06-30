db7b2b426705c710f09462678aba45663c853721
trushithanama <67234326+trushithanama@users.noreply.github.com>
2020-06-30 08:05:58 +0530

    Create A10_copy_list.py

---
+++ A10_copy_list.py
@@ -0,0 +1,12 @@
+#python program to clone and copy a list
+orginal_list = [10, 22, 44, 23, 4]
+new_list = list(orginal_list)
+print(orginal_list)
+print(new_list)
+
+
+
+output
+[10, 22, 44, 23, 4]
+[10, 22, 44, 23, 4]
+>>> 
