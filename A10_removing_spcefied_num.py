b5721827abf130d311a76d8195f5ca5b28fc8f6a
trushithanama <67234326+trushithanama@users.noreply.github.com>
2020-06-30 08:17:34 +0530

    Create A10_removing_spcefied_num.py

---
+++ A10_removing_spcefied_num.py
@@ -0,0 +1,10 @@
+#python program to print the number of a specified list after removeing even numbers 
+num = [7,8,120,25,44,20,27]
+num = [x for x in num if x%2!=0]
+print(num)
+
+
+
+output
+[7, 25, 27]
+>>> 
