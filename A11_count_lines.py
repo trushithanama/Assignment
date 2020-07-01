ba5cb87719c0f666e2790fb15bdae98a9f773967
trushithanama <67234326+trushithanama@users.noreply.github.com>
2020-07-01 08:49:00 +0530

    Create A11_count_lines.py

---
+++ A11_count_lines.py
@@ -0,0 +1,13 @@
+#python program to count number of lines in the list
+def file_lengthy(fname) :
+    with open(fname) as f:
+        for i, 1 in enumerate(f):
+            pass
+    return i + 1
+print("number of lines in the file: ",file_lengthy("test.txt"))
+
+
+
+
+output
+number of lines 5
