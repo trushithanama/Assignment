cb504a6144088860248eadbf02812c11abb6d55a
trushithanama <67234326+trushithanama@users.noreply.github.com>
2020-07-02 20:18:38 +0530

    Create A12_reverse.py

---
+++ A12_reverse.py
@@ -0,0 +1,13 @@
+#python program to reverses the tuple
+def Reverse(tuples) :
+    new_tup = tuples[::-1]
+    return new_tup
+    
+    
+tuples = ('z'.'r','u')
+print(Reverse(tuples))
+    
+
+
+output
+('u','r','z')
