e9d5e675538350af2d56eae77296e5febdfc2399
trushithanama <67234326+trushithanama@users.noreply.github.com>
2020-07-02 20:53:44 +0530

    Create A12_c0lor.py

---
+++ A12_c0lor.py
@@ -0,0 +1,10 @@
+#python program 
+d={'Red':1,'Green':2,'Blue':3}
+for color_key,value in d iteam():
+    print(color_key,'corresponds to', d[color_key])
+    
+    
+    
+    output
+    enter a number:1
+    Red
