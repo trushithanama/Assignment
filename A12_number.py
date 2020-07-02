6db956c9f5af8e5cf8faf80f81ec7d3f833abc08
trushithanama <67234326+trushithanama@users.noreply.github.com>
2020-07-02 20:46:17 +0530

    Create A12_number.py

---
+++ A12_number.py
@@ -0,0 +1,12 @@
+#python program to list a numbers
+1st = []
+num = int(input('how many numbers: '))
+for n in range(num):
+    numbers = int(input('enter number '))
+      1st.append(numbers)
+print("sum of elements in given list is :",sum(1st))
+
+
+
+output
+number = 7
