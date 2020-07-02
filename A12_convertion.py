491c087828b76ff2bdb14aa16bc3593ab226a000
trushithanama <67234326+trushithanama@users.noreply.github.com>
2020-07-02 20:22:00 +0530

    Create A12_convertion.py

---
+++ A12_convertion.py
@@ -0,0 +1,13 @@
+#python program to convert the tuple to string
+def convertTuple(tup) :
+    str = ''.join(tup)
+    return str
+    
+    
+tuple = ('g','e','e','k','s')
+str = convertTuple(tuple)
+print(str)
+
+
+output
+geeks
