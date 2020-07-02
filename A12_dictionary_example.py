e9187e3626b259e42782ce4c0b0ee4b487a4f7e6
trushithanama <67234326+trushithanama@users.noreply.github.com>
2020-07-02 21:02:43 +0530

    Create A12_dictionary_example.py

---
+++ A12_dictionary_example.py
@@ -0,0 +1,11 @@
+Dictionary 
+a dictionary is a collection which is unordered, changeable and indexed. in python dictionaries are written with curly brackets , and they have keys and values.
+example
+    thisdict = {
+    "brand" : "ford",
+    "model" : "mustang",
+    "year" : 1964
+    }
+print  (thidict)
+output
+{'brand': 'ford','model': 'mustang','year' : 1964}
