8212fc202364cc4cca381a4ef4745e42b7e82c5a
trushithanama <67234326+trushithanama@users.noreply.github.com>
2020-07-02 20:27:55 +0530

    Create A12_string.py

---
+++ A12_string.py
@@ -0,0 +1,14 @@
+#tuple of string
+my_data = ("hi","hello","bye")
+print(my_data)
+
+#tuple inside another tuple
+#nested tuple
+my_data4 = ((2,3,4),(1,2,"hi"))
+print(my_data4)
+
+
+output
+('hi', 'hello', 'bye')
+((2, 3, 4), (1, 2, 'hi'))
+>>> 
